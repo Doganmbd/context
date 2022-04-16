6f29084f635e4d950384bff45803c939636d8f94
DMB29 <mbilaldogan90@gmail.com>
2022-04-16 08:43:04 +0300

    incrase

--- src/component/ProductApp.jsx
+++ src/component/ProductApp.jsx
@@ -5,20 +5,35 @@ const ProductApp = () => {
 
 
     const [data, setData] = useState(Data)
+    
 
-    console.log(data[0].product);
+ 
 
+    const handleClickIncrease = (e)=> {
+        console.log(e.target.id)
+        const newData = [...data];
+        newData[e.target.id - 1].price++;
+        setData(newData);
+
+    }
 
   return (
     <div>
-        {data.map((veri)=>{
+        {data.map((veri,index)=>{
             return(
                 <div>
+                    <h1>Product {index +1}</h1>
+                    <h1>{veri.product}</h1>
                     <h1>{veri.price}</h1>
+                    <h1>{veri.quantity}</h1>
+                    <button id={veri.id} onClick = {handleClickIncrease} >increase</button>
+                    
                 </div>
             )
             
         })}
+        
+                    <button>decrease</button>
      
         
     </div>
